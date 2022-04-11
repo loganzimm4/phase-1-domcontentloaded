8989314470fec775839fc1c6b27f3468723447a6
loganzimm4 <101672520+loganzimm4@users.noreply.github.com>
2022-04-11 13:21:43 -0400

    commit tree c780e4da0c4deb4534ea6134ce1f9de789997f36

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+const tag = document.getElementById('text');
+console.log(tag);
+
+document.addEventListener('DOMContentLoaded', loaded);
+console.log(tag);
+function loaded (e) {
+    tag.textContent = 'This is really cool!';
+}
